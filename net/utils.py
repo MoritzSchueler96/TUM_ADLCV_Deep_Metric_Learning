cdec16ffa2dff35286f542e29f7153f368a86d84
lucaeyring <luca.eyring@googlemail.com>
2021-12-22 13:33:49 +0100

    adjusted softmax & added configs

--- net/utils.py
+++ net/utils.py
@@ -172,7 +172,7 @@ def softmax(src, index, dim, dim_size, margin: float = 0.0):
     return out
 
 
-def deterministic_softmax(src, index, batch_size, margin: float = 0.0):
+def deterministic_softmax(src, index, batch_size):
     """
     Deterministic softmax implementation for nodeswise softmax on all neighbours
     Variables here:
@@ -183,7 +183,7 @@ def deterministic_softmax(src, index, batch_size, margin: float = 0.0):
 
     # Calculate max per node
     src2 = rearrange(src, "h (n a)-> h n a", n=batch_size)
-    src_max = torch.clamp(torch.max(src2, dim=-2).values, min=0.0)
+    src_max = torch.max(src2, dim=-2).values
 
     # exp(src - max_node)
     head_indices = torch.arange(src.shape[0]).type(torch.cuda.LongTensor).view(src.shape[0], 1).expand(-1, index.shape[0])
@@ -193,7 +193,7 @@ def deterministic_softmax(src, index, batch_size, margin: float = 0.0):
     # Calculate denominator
     src2 = rearrange(src, "h (n a)-> h n a", n=batch_size)
     denom = torch.sum(src2, dim=-2)
-    denom = (denom + (margin - src_max).exp())[head_indices, index]
+    denom = (denom + 1e-16)[head_indices, index]
 
     out = src / denom
 
